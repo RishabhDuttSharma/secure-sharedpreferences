86310933943e40ba9f634250bbcd2db01554f3e2
Rishabh Dutt Sharma <rishabhduttsharma@gmail.com>
2018-11-26 20:33:03 +0530

    Code for Generating a new KeyPair

--- app/src/main/java/com/learner/secureprefs/RSAKeyStoreHelper.kt
+++ app/src/main/java/com/learner/secureprefs/RSAKeyStoreHelper.kt
@@ -1,7 +1,15 @@
 package com.learner.secureprefs
 
 import android.content.Context
+import android.os.Build
+import android.security.KeyPairGeneratorSpec
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.security.KeyPairGenerator
 import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.PublicKey
+import javax.security.auth.x500.X500Principal
 
 /**
  * Developer: Rishabh Dutt Sharma
@@ -9,22 +17,38 @@ import java.security.KeyStore
  */
 class RSAKeyStoreHelper(context: Context, alias: String) {
 
-    private val keyStoreEntry: KeyStore.PrivateKeyEntry = KeyStore.getInstance("AndroidKeyStore").run {
+    private val keyPair = KeyStore.getInstance("AndroidKeyStore").run {
         // initialize keystore
         load(null)
 
         // generate a new KeyPair, if exists no entry for alias
         if (!containsAlias(alias)) generateKeyPair(context, alias)
 
-        // retrieve and return the Entry
-        getEntry(alias, null) as KeyStore.PrivateKeyEntry
+        // retrieve and return the Keys as Pair<A, B>
+        Pair(getKey(alias, null) as PrivateKey, getCertificate(alias).publicKey as PublicKey)
     }
 
-    val publicProcessor = RSABase64StringProcessor(keyStoreEntry.certificate.publicKey)
+    val publicProcessor = RSABase64StringProcessor(keyPair.second)
 
-    val privateProcessor = RSABase64StringProcessor(keyStoreEntry.privateKey)
+    val privateProcessor = RSABase64StringProcessor(keyPair.first)
 
-    private fun generateKeyPair(context: Context, alias: String) {
-        TODO("Code to Generate a new KeyPair")
+    private fun generateKeyPair(context: Context, alias: String) = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore").run {
+
+
+        val x500Principal = X500Principal("CN=$alias")
+
+        initialize(if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            KeyPairGeneratorSpec.Builder(context)
+                    .setAlias(alias)
+                    .setSubject(x500Principal)
+                    .build()
+        else KeyGenParameterSpec.Builder(alias, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                .setCertificateSubject(x500Principal)
+                .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+                .setDigests(KeyProperties.DIGEST_SHA256)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                .build())
+
+        generateKeyPair()
     }
 }
